4bff181252d570f1498566901e45d563049cf1a8
nrkerkvliet <35312096+nrkerkvliet@users.noreply.github.com>
2018-01-30 15:13:56 -0600

    Add files via upload

--- aspCapstone/Models/Account.cs
+++ aspCapstone/Models/Account.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Security.AccessControl;
 using System.Web;
 
@@ -9,10 +11,16 @@ namespace a_01_carApp.Models
 {
     public class Account
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Enter First Name")]
         public string FName { get; set; }
 
+        [Required(ErrorMessage = "Enter Last Name")]
         public string LName { get; set; }
+
+        public List<Account> ShowAllAccounts { get; set; }
+
     }
 }
--- aspCapstone/Models/Vehicle.cs
+++ aspCapstone/Models/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,16 @@ namespace a_01_carApp.Models
 {
     public class Vehicle
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Enter VIN")]
+        public string Vin { get; set; }
+
+        [Required(ErrorMessage = "Enter Vehicle Make")]
         public string Make { get; set; }
 
+        [Required(ErrorMessage = "Enter Vehicle Model")]
         public string Model { get; set; }
     }
 }
