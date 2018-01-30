7288dcb38738531d2428c6cb4030ac5fcfdded0b
nrkerkvliet <35312096+nrkerkvliet@users.noreply.github.com>
2018-01-30 10:24:50 -0600

    Add files via upload

---
+++ aspCapstone/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace a_01_carApp.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public string FName { get; set; }
+
+        public string LName { get; set; }
+    }
+}
---
+++ aspCapstone/Models/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace a_01_carApp.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+    }
+}
