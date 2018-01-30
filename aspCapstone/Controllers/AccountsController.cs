44c5034c7fa61be855aef2ffc500c39c10fbc477
nrkerkvliet <35312096+nrkerkvliet@users.noreply.github.com>
2018-01-30 15:11:24 -0600

    Add files via upload

---
+++ aspCapstone/Controllers/AccountsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using a_01_carApp.DataAccess;
+using a_01_carApp.Models;
+
+namespace a_01_carApp.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET:/Accounts/
+        [HttpGet]
+        public ActionResult InsertAccount()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult InsertAccount(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                AccountsDataAccess access = new AccountsDataAccess();
+                string result = access.InsertData(account);
+                ViewData["result"] = result;
+                ModelState.Clear();
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("","error in saving data");
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ShowAllAccountDetails()
+        {
+            Account account = new Account();
+            AccountsDataAccess access = new AccountsDataAccess();
+            account.ShowAllAccounts = access.SelectAllData();
+            return View(account);
+        }
+
+    }
+}
