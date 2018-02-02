64e412640237c1fe43d3b2f156c0445bb3b82ffb
nrkerkvliet <35312096+nrkerkvliet@users.noreply.github.com>
2018-02-02 15:43:36 -0600

    Add files via upload

--- aspCapstone/Models/Account.cs
+++ aspCapstone/Models/Account.cs
@@ -20,6 +20,14 @@ namespace a_01_carApp.Models
         [Required(ErrorMessage = "Enter Last Name")]
         public string LName { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
+        [Required(ErrorMessage = "Please enter Date of Birth")]
+        public DateTime DOB { get; set; }
+
+        [Required(ErrorMessage= "Please enter customer address")]
+        [StringLength(50, ErrorMessage = "Only 50 characters allowed for address")]
+        public string Address { get; set; }
+
         public List<Account> ShowAllAccounts { get; set; }
 
     }
