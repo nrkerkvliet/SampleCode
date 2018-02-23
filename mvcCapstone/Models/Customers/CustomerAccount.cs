b571d75e5ba84206970cc850585cdec8c851e69e
nrkerkvliet <35312096+nrkerkvliet@users.noreply.github.com>
2018-02-23 05:42:25 -0600

    Add files via upload

---
+++ mvcCapstone/Models/Customers/CustomerAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace a_01_carApp.Models.Customers
+{
+    public class CustomerAccount
+    {
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int VehicleId { get; set; }
+    }
+}
