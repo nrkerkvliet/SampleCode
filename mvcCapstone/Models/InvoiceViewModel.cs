e6a058e55ca2ada0b2a8501e53b18c6447e2ca02
nrkerkvliet <35312096+nrkerkvliet@users.noreply.github.com>
2018-05-02 21:46:14 -0500

    Add files via upload

---
+++ mvcCapstone/Models/InvoiceViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace a_01_carApp.Models
+{
+    public class InvoiceViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter AccountID")]
+        public int AccountId { get; set; }
+
+        [Required(ErrorMessage = "Enter DocumentId")]
+        public int DocumentId { get; set; }
+
+        [Required(ErrorMessage = "Enter Rate")]
+        public decimal Rate { get; set; }
+
+        [Required(ErrorMessage = "Enter hours worked")]
+        public int HoursWorked { get; set; }
+
+        [Required(ErrorMessage = "Enter Net Total")]
+        public decimal NetTotal { get; set; }
+
+
+        [Required(ErrorMessage = "Enter First Name")]
+        public string FName { get; set; }
+
+        [Required(ErrorMessage = "Enter Last Name")]
+        public string LName { get; set; }
+
+        [Required(ErrorMessage = "Enter Detaills")]
+        public string Details { get; set; }
+
+        [Required(ErrorMessage = "Enter Vehicle Make")]
+        public string Make { get; set; }
+
+        [Required(ErrorMessage = "Enter Vehicle Model")]
+        public string VehicleModel { get; set; }
+
+    }
+}
---
+++ mvcCapstone/Models/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace a_01_carApp.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter VIN")]
+        public string VIN { get; set; }
+
+        [Required(ErrorMessage = "Enter Vehicle Make")]
+        public string Make { get; set; }
+
+        [Required(ErrorMessage = "Enter Vehicle Model")]
+        public string VehicleModel { get; set; }
+
+        [Required(ErrorMessage = "Enter Vehicle Year")]
+        public int VehicleYear { get; set; }
+
+        [Required(ErrorMessage = "Enter Vehicle Color")]
+        public string VehicleColor { get; set; }
+
+        public List<Vehicle> ShowAllVehicles { get; set; }
+
+    }
+}
