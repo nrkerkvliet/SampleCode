43d1e1086bd212e5b6de04bb1023675569ddbe09
nrkerkvliet <35312096+nrkerkvliet@users.noreply.github.com>
2018-05-02 21:45:05 -0500

    Add files via upload

---
+++ mvcCapstone/Controllers/DocumentController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using a_01_carApp.DataAccess;
+using a_01_carApp.Models;
+
+namespace a_01_carApp.Controllers
+{
+    public class DocumentController : Controller
+    {
+        [HttpGet]
+        public ActionResult InsertDocument()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult InsertDocument(Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                DocumentsDataAccess access = new DocumentsDataAccess();
+                string result = access.InsertData(document);
+                ViewData["result"] = result;
+                ModelState.Clear();
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("", "error in saving data");
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ShowAllDocumentDetails()
+        {
+            Document document = new Document();
+            DocumentsDataAccess access = new DocumentsDataAccess();
+            document.ShowAllDocuments = access.SelectAllData();
+            return View(document);
+        }
+
+        [HttpGet]
+        public ActionResult EditDocument(int Id)
+        {
+            Document document = new Document();
+            DocumentsDataAccess access = new DocumentsDataAccess();
+            return View(access.SelectDataById(Id));
+        }
+
+        [HttpPost]
+        public ActionResult EditDocument(Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                DocumentsDataAccess access = new DocumentsDataAccess();
+                string result = access.UpdateData(document);
+                ViewData["result"] = result;
+                ModelState.Clear();
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("", "Error in saving data");
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteDocument(int Id)
+        {
+            Document document = new Document();
+            DocumentsDataAccess accesss = new DocumentsDataAccess();
+            return View(accesss.SelectDataById(Id));
+        }
+
+        [HttpPost]
+        public ActionResult DeleteDocument(Document document)
+        {
+            DocumentsDataAccess access = new DocumentsDataAccess();
+            string result = access.DeleteData(document);
+            ViewData["result"] = result;
+            ModelState.Clear();
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public ActionResult ShowDocumentDetails(int Id)
+        {
+            DocumentViewModel document = new DocumentViewModel();
+            DocumentsDataAccess access = new DocumentsDataAccess();
+            return View(access.SelectSpecificData(Id));
+        }
+
+
+    }
+}
---
+++ mvcCapstone/Controllers/VehiclesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using a_01_carApp.DataAccess;
+using a_01_carApp.Models;
+
+namespace a_01_carApp.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        // GET:/Vehicles/
+        [HttpGet]
+        public ActionResult InsertVehicle()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult InsertVehicle(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                VehiclesDataAccess access = new VehiclesDataAccess();
+                string result = access.InsertData(vehicle);
+                ViewData["result"] = result;
+                ModelState.Clear();
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("", "error in saving data");
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ShowAllVehicleDetails()
+        {
+            Vehicle vehicle = new Vehicle();
+            VehiclesDataAccess access = new VehiclesDataAccess();
+            vehicle.ShowAllVehicles = access.SelectAllData();
+            return View(vehicle);
+        }
+
+        [HttpGet]
+        public ActionResult EditVehicle(int Id)
+        {
+            Vehicle vehicle = new Vehicle();
+            VehiclesDataAccess access = new VehiclesDataAccess();
+            return View(access.SelectDataById(Id));
+        }
+
+        [HttpPost]
+        public ActionResult EditVehicle(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                VehiclesDataAccess access = new VehiclesDataAccess();
+                string result = access.UpdateData(vehicle);
+                ViewData["result"] = result;
+                ModelState.Clear();
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("", "Error in saving data");
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteVehicle(int Id)
+        {
+            Vehicle vehicle = new Vehicle();
+            VehiclesDataAccess accesss = new VehiclesDataAccess();
+            return View(accesss.SelectDataById(Id));
+        }
+
+        [HttpPost]
+        public ActionResult DeleteVehicle(Vehicle vehicle)
+        {
+            VehiclesDataAccess access = new VehiclesDataAccess();
+            string result = access.DeleteData(vehicle);
+            ViewData["result"] = result;
+            ModelState.Clear();
+            return View();
+        }
+    }
+}
