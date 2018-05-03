2e32ea7afb8ad37cf3e48406801f5f5413cda278
nrkerkvliet <35312096+nrkerkvliet@users.noreply.github.com>
2018-05-02 21:45:41 -0500

    Add files via upload

---
+++ mvcCapstone/Models/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace a_01_carApp.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter First Name")]
+        public string FName { get; set; }
+
+        [Required(ErrorMessage = "Enter Last Name")]
+        public string LName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
+        [Required(ErrorMessage = "Please enter Date of Birth")]
+        public DateTime DOB { get; set; }
+
+        [Required(ErrorMessage= "Please enter customer address")]
+        [StringLength(50, ErrorMessage = "Only 50 characters allowed for address")]
+        public string Address { get; set; }
+
+        public List<Account> ShowAllAccounts { get; set; }
+
+    }
+}
---
+++ mvcCapstone/Models/Document.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace a_01_carApp.Models
+{
+    public class Document
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Description")]
+        public string Details { get; set; }
+
+        [Required(ErrorMessage = "Enter  VehicleId")]
+        public int VehicleId { get; set; }
+
+
+        public List<Document> ShowAllDocuments { get; set; }
+
+    }
+}
