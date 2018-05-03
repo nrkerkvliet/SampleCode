bc5fc08dbcc23a641a1ef9c9a8700b61ac3979ea
nrkerkvliet <35312096+nrkerkvliet@users.noreply.github.com>
2018-05-02 21:44:45 -0500

    Add files via upload

--- mvcCapstone/Controllers/InvoicesController.cs
+++ mvcCapstone/Controllers/InvoicesController.cs
@@ -4,42 +4,99 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using a_01_carApp.DataAccess;
-using a_01_carApp.Models.Invoices;
-using a_01_carApp.Models.Vehicles;
+using a_01_carApp.Models;
 
-namespace a_01_carApp.Controllers.Invoices
+namespace a_01_carApp.Controllers
 {
     public class InvoicesController : Controller
     {
         [HttpGet]
-        public ActionResult ShowAllInvoices()
+        public ActionResult InsertInvoice()
         {
-            var invoice = new Invoice();
-            InvoicesDataAccess access = new InvoicesDataAccess();
-            invoice.ShowAllInvoices = access.SelectAllInvoices();
-            return PartialView(invoice);
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult InsertInvoice(Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                InvoicesDataAccess access = new InvoicesDataAccess();
+                string result = access.InsertData(invoice);
+                ViewData["result"] = result;
+                ModelState.Clear();
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("", "error in saving data");
+                return View();
+            }
         }
 
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult ShowAllInvoiceDetails()
         {
-            return View(new Invoice());
+            Invoice invoice = new Invoice();
+            InvoicesDataAccess access = new InvoicesDataAccess();
+            invoice.ShowAllInvoices = access.SelectAllData();
+            return View(invoice);
+        }
+
+        [HttpGet]
+        public ActionResult EditInvoice(int Id)
+        {
+            Invoice invoice = new Invoice();
+            InvoicesDataAccess access = new InvoicesDataAccess();
+            return View(access.SelectDataById(Id));
         }
 
         [HttpPost]
-        public JsonResult InsertInvoice(Invoice objInvoice)
+        public ActionResult EditInvoice(Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                InvoicesDataAccess access = new InvoicesDataAccess();
+                string result = access.UpdateData(invoice);
+                ViewData["result"] = result;
+                ModelState.Clear();
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("", "Error in saving data");
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteInvoice(int Id)
+        {
+            Invoice invoice = new Invoice();
+            InvoicesDataAccess accesss = new InvoicesDataAccess();
+            return View(accesss.SelectDataById(Id));
+        }
+
+        [HttpPost]
+        public ActionResult DeleteInvoice(Invoice invoice)
         {
             InvoicesDataAccess access = new InvoicesDataAccess();
+            string result = access.DeleteData(invoice);
+            ViewData["result"] = result;
+            ModelState.Clear();
+            return View();
+        }
 
-            try
-            {
-                access.InsertInvoice(objInvoice);
-                return Json(objInvoice, JsonRequestBehavior.AllowGet);
-            }
-            catch(Exception ex)
-            {
-                return null;
-            }
+
+
+        [HttpGet]
+        public ActionResult ShowInvoiceDetails(int Id)
+        {
+            InvoiceViewModel invoice = new InvoiceViewModel();
+            InvoicesDataAccess access = new InvoicesDataAccess();
+            return View(access.SelectSpecificData(Id));
         }
     }
 }
